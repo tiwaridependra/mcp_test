d91dff9bbac22b059a674feca2718c17854100d3
Dependra Tiwari <tiwaridependra1410@gmail.com>
2026-05-26 12:28:17 +0530

    Initail commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastmcp import FastMCP
+import random 
+import json 
+
+
+mcp=FastMCP("Simple calculator Server")
+
+@mcp.tool
+def add(a :int,b:int)->int:
+    '''Adds the two given numbers
+    Args : a-> first number
+           b->second number 
+
+    returns:
+         The Sum of a and b
+
+    '''
+    return a+b
+
+@mcp.tool
+def random_number(min_val:int=1,max_val:int=100)->int:
+    """ return a random nuymber between min_val and max_val"""
+    return  random.randint(min_val,max_val)
+
+
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http",host="0.0.0.0",port=8000)
